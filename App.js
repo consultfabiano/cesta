092676601acf521c2e5d1e65494ccb01075a53df
fabiano <fabianojp06@gmail.com>
2022-06-08 13:14:11 -0300

    alter 03.2

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
-import { StatusBar ,SafeAreaView } from 'react-native';
+import { StatusBar ,SafeAreaView, View } from 'react-native';
 import Cesta from './src/telas/Cesta';
+import { useFonts, Montserrat_700Bold, Montserrat_500Medium } from '@expo-google-fonts/montserrat';
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    "MontserratRegular": Montserrat_500Medium,
+    "MontserratBold": Montserrat_700Bold,
+  });
+
+  if (!fonteCarregada) {
+    <View />
+  }
+
   return (
     <SafeAreaView >
       <StatusBar />
